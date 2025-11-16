8d7ec96b69b9a4e70ef109f24407e9acb9673b03
Jay1156 <132078409+Jay1156@users.noreply.github.com>
2025-11-16 13:32:55 +0530

    Refactor DDL for bronze schema and remove bulk inserts
    Removed bulk insert statements and added table creation logic.

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -1,63 +1,62 @@
-CREATE OR ALTER PROCEDURE bronze.load_bronze AS 
-BEGIN
-	TRUNCATE TABLE bronze.crm_cust_info;
-	BULK INSERT bronze.crm_cust_info   --Bulk inserting data from original csv file
-	FROM 'E:\30SqlAdv\Data Warehouse project\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
-	WITH (                                        ----Now the sql dont know from where the data start, so this with functions help with that
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',                         --File seperated , like how the value are seperate
-	TABLOCK
-	);
+IF OBJECT_ID ('bronze.crm_cust_info', 'U') IS NOT NULL
+		DROP TABLE bronze.crm_cust_info
 
+CREATE TABLE bronze.crm_cust_info(
+cust_id INT,
+cst_key NVARCHAR(50),
+cst_firstname VARCHAR(50),
+cst_lastname VARCHAR(50),
+cst_marital_status NVARCHAR(50),
+cst_gndr VARCHAR(50),
+cst_create_date DATE
+);
 
+IF OBJECT_ID ('bronze.crm_prd_info', 'U') IS NOT NULL
+		DROP TABLE bronze.crm_prd_info
+CREATE TABLE bronze.crm_prd_info(
+prd_id INT,
+prd_key NVARCHAR(50),
+prd_nm VARCHAR(50),
+prd_cost INT,
+prd_line NVARCHAR(50),
+prd_start_dt DATETIME,
+prd_end_dt DATETIME
+);
 
-	TRUNCATE TABLE bronze.crm_prd_info;
-	BULK INSERT bronze.crm_prd_info
-	FROM 'E:\30SqlAdv\Data Warehouse project\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
-	WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-	);
+IF OBJECT_ID ('bronze.crm_sales_details', 'U') IS NOT NULL
+		DROP TABLE bronze.crm_sales_details
+CREATE TABLE bronze.crm_sales_details (
+sls_ord_num NVARCHAR(50),
+sls_prd_key NVARCHAR(50),
+sls_cust_id INT,
+sls_order_dt INT,
+sls_ship_dt INT,
+sls_due_dt INT,
+sls_sales INT,
+sls_quantity INT,
+sls_price INT
+);
 
+IF OBJECT_ID ('bronze.erp_cust_az12', 'U') IS NOT NULL
+		DROP TABLE bronze.erp_cust_az12
+CREATE TABLE bronze.erp_cust_az12 (
+cid NVARCHAR(50),
+bdate DATE,
+gen NVARCHAR(50)
+);
 
-	TRUNCATE TABLE bronze.crm_sales_details;
-	BULK INSERT bronze.crm_sales_details
-	FROM 'E:\30SqlAdv\Data Warehouse project\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
-	WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-	);
+IF OBJECT_ID ('bronze.erp_loc_a101', 'U') IS NOT NULL
+		DROP TABLE bronze.erp_loc_a101
+CREATE TABLE bronze.erp_loc_a101 (
+cid NVARCHAR(50),
+cntry NVARCHAR(50)
+);
 
-
-	TRUNCATE TABLE bronze.erp_cust_az12;
-	BULK INSERT bronze.erp_cust_az12
-	FROM 'E:\30SqlAdv\Data Warehouse project\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
-	WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-	);
-
-
-	TRUNCATE TABLE bronze.erp_loc_a101;
-	BULK INSERT bronze.erp_loc_a101
-	FROM 'E:\30SqlAdv\Data Warehouse project\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
-	WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-	);
-
-
-	TRUNCATE TABLE bronze.erp_px_cat_g1v2;
-	BULK INSERT bronze.erp_px_cat_g1v2
-	FROM 'E:\30SqlAdv\Data Warehouse project\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
-	WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-	);
-
-END
+IF OBJECT_ID ('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
+		DROP TABLE bronze.erp_px_cat_g1v2
+CREATE TABLE bronze.erp_px_cat_g1v2 (
+id NVARCHAR(50),
+cat NVARCHAR(50),
+subcat NVARCHAR(50),
+maintenance NVARCHAR(50)
+);
